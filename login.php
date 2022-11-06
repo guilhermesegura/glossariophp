faee460b1a0a7bb24906210c8c01ee4e9a7431d5
wallace.lins19@gmail.com <wallace.lins19@gmail.com>
2022-11-06 01:18:09 -0300

    Wallace Tela de Login atualizado

--- login.php
+++ login.php
@@ -1,19 +1,54 @@
-<link href="./css/login.css" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="./css/login.css" rel="stylesheet">
+</head>
+<body>
+      
+
+
+    
 <section class="sec1" > 
-    <form action="verificar-usuario.php" method="post">
-        <div class="div1"  >
-            <input type="text" placeholder="Usuário" name="txUsuario" />
+        
+
+    <div class="divpai">
+
+            <div class= "txtLogin"> Login </div>
+            <form action="verificar-usuario.php" method="post">
+
+        <div class ="div1" >
+
+            <input type="text" placeholder="Usuário" name="txUsuario" class="email" />
+
         </div>
 
-        <div class="div2">
-            <input type="password" placeholder="Senha" name="txSenha"/>
+        <div class="div2">           
+            
+             <input type="password" placeholder="Senha" name="txSenha" class="senha"    />
+
         </div >      
 
-        <div class="butao" >
-            <input type="submit" value="Login" />
+        <div class = "divbotao"  >
+
+             <input type="submit" value="Login" class="butao" />
+
         </div>
-    </form>
+
+            </form>
+
+    </div>   
+
 </section>
+
+
+
+
+
+    
 <?php
 
     if (!empty($_GET['erro'])) {
@@ -21,3 +56,8 @@
     }
 
 ?>
+
+    
+
+</body>
+</html>
