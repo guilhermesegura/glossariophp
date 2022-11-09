78b76b8b557926c725a4dfc00dc65f2dc7c8a7a1
anguecalmon <calmon.principal@gmail.com>
2022-11-09 20:24:25 -0300

    adiciona o arquivo php do glossario

--- glossario.php
+++ glossario.php
@@ -1,25 +1,66 @@
+<<<<<<< HEAD
+<?php include('templates/head.php'); ?>
+=======
 <link rel="stylesheet" href="css/header.css">
 <link rel="stylesheet" href="css/estilo_glossario.css">
 <link rel="stylesheet" href="css/home.css">
+>>>>>>> 850658bafc66dc50f525717b6e6a7f0c1dd93add
 
-<div class="header">
-    <ul>
-        <a href="home.php" class="logo">Projeto Glossário</a>
-        <div class="header-right">
-            <li><a href="home.php">Home</a></li>
-            <li><a class="active" href="glossario.php">Glossário</a></li>
-            <li><a href="login.php">Login</a></li>
-        </div>
-    </ul>
-</div>
+<section class="glossario">
 
-<section class="secao">
+    <a href="palavra_letra.php?letra=A"> A </a>
 
-    <a class="link" href="palavra-letra.php?letra=A"> A </a>
-    <a class="link" href="palavra-letra.php?letra=B"> B </a>
-    <a class="link" href="palavra-letra.php?letra=C"> C </a>
-    <a class="link" href="palavra-letra.php?letra=D"> D </a>
-    <a class="link" href="palavra-letra.php?letra=M"> M </a>
-    <a class="link" href="palavra-letra.php?letra=T"> T </a>
+    <a href="palavra_letra.php?letra=B"> B </a>
+
+    <a href="palavra_letra.php?letra=C"> C </a>
+
+    <a href="palavra_letra.php?letra=D"> D </a>
+
+    <a href="palavra_letra.php?letra=E"> E </a>
+
+    <a href="palavra_letra.php?letra=F"> F </a>
+
+    <a href="palavra_letra.php?letra=G"> G </a>
+
+    <a href="palavra_letra.php?letra=H"> H </a>
+
+    <a href="palavra_letra.php?letra=I"> I </a>
+
+    <a href="palavra_letra.php?letra=J"> J </a>
+
+    <a href="palavra_letra.php?letra=K"> K </a>
+
+    <a href="palavra_letra.php?letra=L"> L </a>
+
+    <a href="palavra_letra.php?letra=M"> M </a>
+
+    <a href="palavra_letra.php?letra=N"> N </a>
+
+    <a href="palavra_letra.php?letra=O"> O </a>
+
+    <a href="palavra_letra.php?letra=P"> P </a>
+
+    <a href="palavra_letra.php?letra=Q"> Q </a>
+
+    <a href="palavra_letra.php?letra=R"> R </a>
+
+    <a href="palavra_letra.php?letra=S"> S </a>
+
+    <a href="palavra_letra.php?letra=T"> T </a>
+
+    <a href="palavra_letra.php?letra=U"> U </a>
+
+    <a href="palavra_letra.php?letra=V"> V </a>
+
+    <a href="palavra_letra.php?letra=W"> W </a>
+
+    <a href="palavra_letra.php?letra=X"> X </a>
+
+    <a href="palavra_letra.php?letra=Y"> Y </a>
+
+    <a href="palavra_letra.php?letra=Z"> Z </a>
+
 
 </section>
+
+<?php include("templates/footer.php"); ?>
