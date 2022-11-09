5ef18c858c6ee312824496b635f7d0e11bc55e28
Guilhermesegura <guilhermesegura26@gmail.com>
2022-11-09 20:32:12 -0300

    Arruma os nomes dos links e adiciona o header

--- glossario.php
+++ glossario.php
@@ -1,10 +1,26 @@
-<<<<<<< HEAD
-<?php include('templates/head.php'); ?>
-=======
-<link rel="stylesheet" href="css/header.css">
-<link rel="stylesheet" href="css/estilo_glossario.css">
-<link rel="stylesheet" href="css/home.css">
->>>>>>> 850658bafc66dc50f525717b6e6a7f0c1dd93add
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Glossário</title>
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/estilo_glossario.css">
+    <link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+
+<div class="header">
+    <ul>
+        <a href="home.php" class="logo">Projeto Glossário</a>
+        <div class="header-right">
+            <li><a href="home.php">Home</a></li>
+            <li><a href="glossario.php">Glossário</a></li>
+            <li><a class="active" href="login.php">Login</a></li>
+        </div>
+    </ul>
+</div>
 
 <section class="glossario">
 
@@ -63,4 +79,7 @@
 
 </section>
 
-<?php include("templates/footer.php"); ?>
+<?php include("footer.php"); ?>
+
+</body>
+</html>
